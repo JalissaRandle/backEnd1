97a9cdb8ee2d507d4f746161982bc361bd854afe
JalissaRandle <Jalissar18@Gmail.com>
2022-03-21 14:26:34 -0500

    second commit

--- _build/Server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const cors = require("cors");
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-const SERVER_PORT = 4000
-app.listen(SERVER_PORT, () => console.log(`server running port ${SERVER_PORT}`));
